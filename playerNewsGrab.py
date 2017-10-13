05e0277f9c8bbaa9497403d261e8d4a9cfd4cb4d
Glenn Raskovich <graskovi@users.noreply.github.com>
2017-10-12 22:37:51 -0700

    Update playerNewsGrab.py

--- playerNewsGrab.py
+++ playerNewsGrab.py
@@ -6,11 +6,21 @@ from bs4 import BeautifulSoup
 
 playerNewsArray = []
 newsObject = {}
-# Grab player web page, grab the html, crab the divs from that html, and
-# Grab the recent news about the player from that div.
-playerHTML = requests.get('http://www.rotoworld.com/recent/nfl/11292/thomas-duarte')
+
+# Grab player web page
+print "Remember to include the http portions of the webpage URL"
+playerURL = raw_input("Type or copy/paste rotoworld URL of player here: ")
+# Deprecated URL of player due to hardcoding, bad practice, keeping comment as note of acceptable URL
+#playerHTML = requests.get('http://www.rotoworld.com/recent/nfl/11292/thomas-duarte')
+playerHTML = requests.get(playerURL)
+
+# Grab text from player if URL from user was valid
 htmlData = playerHTML.text
+
+# Grab the HTML div sections from the webpage
 divData = [div for div in BeautifulSoup(htmlData)("div")]
+
+# Collect recent news about the player from each div
 for div in divData:
     divClass = div.get('class');
     if ( divClass ):
@@ -23,6 +33,7 @@ for div in divData:
             playerNewsArray.append( newsObject )
             newsObject = {}
 
+# Iterate through player statistics and print relevant info
 for news in playerNewsArray:
     print "Report:" + news['report']
     print "Impact:\n" + news['impact']
